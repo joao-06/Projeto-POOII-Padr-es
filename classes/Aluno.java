83f5939fb5c648b008318885bf3486b680ca0d4a
joao-06 <66530247+joao-06@users.noreply.github.com>
2025-03-07 15:36:20 -0300

    Update Aluno.java

--- classes/Aluno.java
+++ classes/Aluno.java
@@ -4,17 +4,20 @@ public class Aluno extends Pessoa {
 
     private String matricula;
     private Double notaRecuperacao;
+    private EstadoAluno estado;
 
     public Aluno() {
         super();
         this.matricula = "";
         this.notaRecuperacao = 0.0;
+        this.estado = new Reprovado(); 
     }
 
     public Aluno(String nome, String cpf, String telefone, String endereco, String matricula, Double notaRecuperacao) {
         super(nome, cpf, telefone, endereco);
         this.matricula = matricula != null ? matricula : "";
         this.notaRecuperacao = notaRecuperacao != null ? notaRecuperacao : 0.0;
+        this.estado = new Reprovado(); 
     }
 
     public String getMatricula() {
@@ -31,6 +34,15 @@ public class Aluno extends Pessoa {
 
     public void setNotaRecuperacao(Double notaRecuperacao) {
         this.notaRecuperacao = notaRecuperacao != null ? notaRecuperacao : 0.0;
+        estado.verificarEstado(this); // Verifica se o estado precisa mudar
+    }
+
+    public void setEstado(EstadoAluno estado) {
+        this.estado = estado;
+    }
+
+    public void exibirSituacao() {
+        estado.exibirSituacao();
     }
 
     @Override
@@ -41,10 +53,12 @@ public class Aluno extends Pessoa {
         System.out.println("Telefone: " + getTelefone());
         System.out.println("Matrícula: " + matricula);
         System.out.println("Nota de Recuperação: " + notaRecuperacao);
+        exibirSituacao(); // Exibe a situação atual do aluno
     }
 
     public void exibirHistorico() {
         System.out.println("Histórico do aluno " + (getNome() != null ? getNome() : "Desconhecido") + ":");
         System.out.println("Nota de Recuperação: " + notaRecuperacao);
+        exibirSituacao(); // Exibe a situação atual do aluno
     }
 }
