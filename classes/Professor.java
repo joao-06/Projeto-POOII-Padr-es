b09bb4f30b1e279619f45fe84b991be3b14475af
joao-06 <66530247+joao-06@users.noreply.github.com>
2025-03-07 15:38:38 -0300

    Update Professor.java

--- classes/Professor.java
+++ classes/Professor.java
@@ -5,15 +5,18 @@ public class Professor extends Pessoa {
     private String siape;
     private boolean coordenador = false;
     private String senha;
+    private EstadoProfessor estado;
 
     public Professor() {
         super();
+        this.estado = new Inativo(); 
     }
 
     public Professor(String nome, String cpf, String telefone, String endereco, String siape, String senha) {
         super(nome, cpf, telefone, endereco);
         this.siape = siape;
         this.senha = senha;
+        this.estado = new Inativo(); 
     }
 
     public String getSiape() {
@@ -40,10 +43,23 @@ public class Professor extends Pessoa {
         this.coordenador = coordenador;
     }
 
+    public void setEstado(EstadoProfessor estado) {
+        this.estado = estado;
+    }
+
+    public void exibirEstado() {
+        estado.exibirEstado();
+    }
+
+    public void verificarEstado() {
+        estado.verificarEstado(this);
+    }
+
     @Override
     public void exibirDados() {
         super.exibirDados();
         System.out.println("SIAPE: " + siape);
         System.out.println("Coordenador: " + (coordenador ? "Sim" : "Não"));
+        exibirEstado(); // Exibe o estado atual do professor
     }
 }
