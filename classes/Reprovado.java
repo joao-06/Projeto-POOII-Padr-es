a94faaa2fa7b032017ab77f9ebf6f8c1575d7edd
joao-06 <66530247+joao-06@users.noreply.github.com>
2025-03-08 12:14:27 -0300

    Update Reprovado.java

--- classes/Reprovado.java
+++ classes/Reprovado.java
@@ -3,17 +3,7 @@ package classes;
 public class Reprovado implements IEstadoAluno{
 
     public void addNota(AlunoTurma alunoTurma, Nota nota){
-        @Override
-    public void exibirSituacao() {
-        System.out.println("Aluno está Reprovado.");
-    }
-
-    @Override
-    public void verificarEstado(AlunoTurma nota) {
-        if (aluno.getNotaRecuperacao() >= 2.5) {
-            aluno.setEstado(new Recuperacao());
-        }
-    }
+        System.out.println(x:"Aluno Reprovado");
     }
     
 }
